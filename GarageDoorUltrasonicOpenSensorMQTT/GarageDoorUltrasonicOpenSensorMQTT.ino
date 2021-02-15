db4c7fe7d604d34101f9f1ea7791ddb5e9c2200e
Jim Brooks <james.brooks@verizonmedia.com>
2021-02-15 16:05:14 -0500

    integrate WIFIConfigurator

--- GarageDoorUltrasonicOpenSensorMQTT/GarageDoorUltrasonicOpenSensorMQTT.ino
+++ GarageDoorUltrasonicOpenSensorMQTT/GarageDoorUltrasonicOpenSensorMQTT.ino
@@ -1,44 +1,41 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <PubSubClient.h>
-#include <SimpleTimer.h>
+#include <EEPROM.h>
+#include <WIFIConfigurator.h>
 
-/* Set these to your desired credentials. */
-const char *ssid = "N9I73"; //Enter your WIFI ssid
-const char *password = "1801523b3b"; //Enter your WIFI password
-const char* mqttServer = "192.168.1.200";
-const int mqttPort = 1883;
-const char* mqttUser = "mqtt";
-const char* mqttPassword = "cutieserver";
-const char* mqttCmndTopic = "garagedoor/cmd";
-const char* mqttStateTopic = "garagedoor/state";
+const String configLabels = "MQTT Server Host|MQTT Port|MQTT User|MQTT Password|MQTT Button Topic|MQTT State Topic|Sensor Read Interval (ms)|Max Open Distance (cm)";
+
+char* mqttServer;
+int mqttPort;
+char* mqttUser;
+char* mqttPassword;
+char* mqttButtonTopic;
+char* mqttStateTopic;
+int checkIntervalMillis;
+int maxOpenDistance;
 
 const int trigPin = 2;  //D4
 const int echoPin = 0;  //D3
-const int relayPin = 14; //D5
+const int relayPin = 4; //D2
 
 unsigned long lastChecked = 0;
-const int checkIntervalMillis = 5000; //level check Interval
-unsigned long lastHeartbeat = 0;
-const int heartbeatIntervalMillis = 10000; //distance always reported to MQTT topic on heartbeat interval
-unsigned long now = 0;
-const int openMaxDistance = 50;
 bool isOpen = false;
+bool configReady = false;
 
-//SimpleTimer timer;
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
+WIFIConfigurator configurator(configLabels);
+char configData[500];
+
 void connectMQTT() {
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("garagedooropener", mqttUser, mqttPassword )) {
-      Serial.println("connected to MQTT server");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
+  Serial.println("Connecting to MQTT...");
+  if (client.connect("garagedooropener", mqttUser, mqttPassword )) {
+    Serial.println("connected to MQTT server");
+  } else {
+    Serial.print("failed with state ");
+    Serial.println(client.state());
   }
 }
 
@@ -61,35 +58,32 @@ int getDistance() {
   return distance;
 }
 
-
 void publish(char* payload) {
   connectMQTT();
   client.publish(mqttStateTopic, payload);
 }
 
-void checkIfItWorked(bool desireOpen) {
-  checkDoor(true);
-  if (isOpen != desireOpen) {
-    pushButton();
-    //timer.setTimeout(10000, isOpen(true)); 
-  }
-}
-
 void handleCommand(char* topic, byte* payload, unsigned int length) {
   Serial.println("handleCommand()");
-  
-  if (!strncmp((char *)payload, "OPEN", length)) {
-    openDoor();
+  Serial.println(topic);
+  char myNewArray[length+1];
+  for (int i=0;i<length;i++)
+  {
+    myNewArray[i] = (char)payload[i];
   }
-  if (!strncmp((char *)payload, "CLOSE", length)) {
-    closeDoor();
+  myNewArray[length] = NULL;
+  Serial.print("Payload: ");
+  Serial.println(myNewArray);  // null terminated array
+  if (!strncmp((char *)payload, "PUSHBUTTON", length)) {
+    pushButton();
   }
 }
 
 bool checkDoor(bool reportIt) {
   int dist = getDistance();
-  isOpen = dist <= openMaxDistance;
-  Serial.print("in checkDoor(); isOpen: ");
+  isOpen = dist <= maxOpenDistance;
+  Serial.println("checkDoor()");
+  Serial.print("isOpen: ");
   Serial.print(isOpen);
   Serial.print("  Distance: ");
   Serial.println(dist);
@@ -107,80 +101,86 @@ bool checkDoor(bool reportIt) {
 void pushButton() {
   Serial.println("pushButton()");
   digitalWrite(relayPin, HIGH);
-  delay(2000);
+  delay(500);
   digitalWrite(relayPin, LOW);
-}
-
-void openDoor() {
-  Serial.println("openDoor()");
-  checkDoor(true);
-  if (! isOpen) {
-    Serial.println("door is closed, pushing button");
-    pushButton();
-    //timer.setTimeout(10000, checkIfItWorked(true)); 
-  }
-}
-
-void closeDoor() {
-  Serial.println("closeDoor()");
-  checkDoor(true);
-  if (isOpen) {
-    Serial.println("door is open, pushing button");
-    pushButton();
-    //timer.setTimeout(10000, checkIfItWorked(false)); 
-  }
 }
 
 void setup() {
  
- delay(3000);
- Serial.begin(115200);
- Serial.println();
- Serial.print("Configuring access point...");
- WiFi.begin(ssid, password);
+  delay(3000);
+  Serial.begin(115200);
+  Serial.println(); 
 
- while (WiFi.status() != WL_CONNECTED) {
-   delay(500);
-   Serial.print(".");
- }
+  configurator.begin();
 
- Serial.println("");
- Serial.println("WiFi connected");
- Serial.println("IP address: ");
- Serial.println(WiFi.localIP()); 
+  EEPROM.begin(500);
+  EEPROM.get(0, configData);
+  Serial.println(configData);
 
- client.setServer(mqttServer, mqttPort);
- client.setCallback(handleCommand);
+  while (true) {
+    char* ssid = strtok(configData, "|");
+    if (ssid == NULL) break;
+    char* wifipassword = strtok(NULL, "|");
+    if (wifipassword == NULL) break;
+    char* hostname = strtok(NULL, "|");
+    if (hostname == NULL) break;
+    mqttServer = strtok(NULL, "|");
+    if (mqttServer == NULL) break;
+    mqttPort = atoi(strtok(NULL, "|"));
+    if (mqttPort == NULL) break;
+    mqttUser = strtok(NULL, "|");
+    if (mqttUser == NULL) break;
+    mqttPassword = strtok(NULL, "|");
+    if (mqttPassword == NULL) break;
+    mqttButtonTopic = strtok(NULL, "|");
+    if (mqttButtonTopic == NULL) break;
+    mqttStateTopic = strtok(NULL, "|");
+    if (mqttStateTopic == NULL) break;
+    checkIntervalMillis = atoi(strtok(NULL, "|"));
+    if (checkIntervalMillis == NULL) break;
+    maxOpenDistance = atoi(strtok(NULL, "|"));
+    if (maxOpenDistance == NULL) break; 
 
- pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
- pinMode(echoPin, INPUT); // Sets the echoPin as an Input
- pinMode(relayPin, OUTPUT);
+    client.setServer(mqttServer, mqttPort);
+    client.setCallback(handleCommand);
 
- connectMQTT();
- checkDoor(true);
- client.subscribe(mqttCmndTopic);
+    connectMQTT();
+    checkDoor(true);
+    client.subscribe(mqttButtonTopic);
+
+    configReady = true;
+    Serial.println("config ready!");
+    break;
+  } 
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
 
- client.loop(); //required to push msgs to handleCommand() callback
+  configurator.handleClient();
  
- now = millis();
+  unsigned long now = millis();
 
- if (now - lastChecked > checkIntervalMillis || lastChecked > now) { //millis() will cycle back to zero every 50 days
-   Serial.println("regular check of door state...");
-   bool lastState = isOpen;
-   checkDoor(false);
-   client.subscribe(mqttCmndTopic);
-   if (isOpen != lastState) {
-     Serial.println("door changed state, probably because of manual button press");
-     if (isOpen) {
-       publish("OPEN");
-     } else {
-       publish("CLOSED");
-     }
-   }
-   lastChecked = now;
- }
- 
+  if (configReady) {
+    client.loop(); //required to push msgs to handleCommand() callback
+
+    if (now - lastChecked > checkIntervalMillis || lastChecked > now) { //millis() will cycle back to zero every 50 days
+      Serial.println("regular check of door state...");
+      bool lastState = isOpen;
+      checkDoor(false);
+      //client.subscribe(mqttButtonTopic);
+      if (isOpen != lastState) {
+        Serial.println("door changed state, probably because of manual button press");
+        if (isOpen) {
+          publish("OPEN");
+        } else {
+          publish("CLOSED");
+        }
+      }
+      lastChecked = now;
+    }
+  }
 } 
